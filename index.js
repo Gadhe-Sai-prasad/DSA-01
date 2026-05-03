4c63d8777f29ce8c933b26c3411c21fe27271e8e
Gadhe-sai prasad <saiprasadgadhe@gmail.com>
2026-05-03 13:02:24 +0530

    addition

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log("hello world");
-console.log("welcome to node js ");
-console.log("sia prasad");
-console.log("DSA");
+// console.log("hello world");
+// console.log("welcome to node js ");
+// console.log("sia prasad");
+// console.log("DSA");
+
+const a = 10;
+const b = 20;
+
+const sum = a + b;
+
+console.log("the sum of a and b is :" + sum);
