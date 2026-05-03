f575451e836552855c20be57a09f653bfbad5a36
Gadhe-sai prasad <saiprasadgadhe@gmail.com>
2026-05-03 13:07:44 +0530

    Diff

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 // console.log("sia prasad");
 // console.log("DSA");
 
-const a = 10;
-const b = 20;
+// const a = 10;
+// const b = 20;
 
-const sum = a + b;
+// const sum = a + b;
 
-console.log("the sum of a and b is :" + sum);
+// console.log("the sum of a and b is :" + sum);
+
+const a = 20;
+const b = 10;
+
+Diff = a - b ;
+console.log(Diff);
